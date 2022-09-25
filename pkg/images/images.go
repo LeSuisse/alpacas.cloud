fca8c7f14c716c5e6b04dc73fd3cdeda33561421
Thomas Gerbet <thomas@gerbet.me>
2022-09-25 13:51:30 +0200

    Remove usages of deprecated io/ioutil

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -3,8 +3,8 @@ package images
 import (
 	"errors"
 	"github.com/h2non/bimg"
-	"io/ioutil"
 	"math"
+	"os"
 	"path"
 	"path/filepath"
 )
@@ -31,7 +31,7 @@ func (e *RequestedSizeTooBigError) Error() string {
 }
 
 func New(imagesFolderPath string) (Images, error) {
-	files, err := ioutil.ReadDir(imagesFolderPath)
+	files, err := os.ReadDir(imagesFolderPath)
 	if err != nil {
 		return nil, err
 	}
@@ -39,7 +39,7 @@ func New(imagesFolderPath string) (Images, error) {
 	var im Images
 
 	for _, file := range files {
-		if file.Mode().IsRegular() && filepath.Ext(file.Name()) == ".jpg" {
+		if file.Type().IsRegular() && filepath.Ext(file.Name()) == ".jpg" {
 			im = append(im, path.Join(imagesFolderPath, file.Name()))
 		}
 	}
@@ -68,7 +68,7 @@ func (images Images) Get(requestedOpts ImageOpts) (*OutputImage, error) {
 
 func (images Images) getResizedImage(requestedOpts ImageOpts) (*OutputImage, error) {
 	imagePath := images[randomSource.Intn(len(images))]
-	inputBuf, err := ioutil.ReadFile(imagePath)
+	inputBuf, err := os.ReadFile(imagePath)
 	if err != nil {
 		return nil, err
 	}
@@ -102,7 +102,7 @@ func (images Images) getResizedImage(requestedOpts ImageOpts) (*OutputImage, err
 	scale := math.Min(scaleWidth, scaleHeight)
 
 	if scale != 1 {
-		_, err = image.Resize(int(float64(imageSize.Width) * scale), int(float64(imageSize.Height) * scale))
+		_, err = image.Resize(int(float64(imageSize.Width)*scale), int(float64(imageSize.Height)*scale))
 		if err != nil {
 			return nil, err
 		}
@@ -131,7 +131,7 @@ func (images Images) GetPlaceHolder(requestedOpts ImageOpts) (*OutputImage, erro
 
 func (images Images) getPlaceHolderImage(requestedOpts ImageOpts) (*OutputImage, error) {
 	imagePath := images[randomSource.Intn(len(images))]
-	inputBuf, err := ioutil.ReadFile(imagePath)
+	inputBuf, err := os.ReadFile(imagePath)
 	if err != nil {
 		return nil, err
 	}
