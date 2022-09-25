e59b40d5a0e5e915ad9f3aa426cf364bffc97e32
Thomas Gerbet <thomas@gerbet.me>
2022-09-25 14:01:04 +0200

    Fix build of the web app
    ESBuild deps optimization is used at build time [0] which appears to fix the
    build of Swagger.
    
    [0] https://vitejs.dev/guide/migration.html#using-esbuild-deps-optimization-at-build-time

--- cmd/alpacascloud/web/vite.config.ts
+++ cmd/alpacascloud/web/vite.config.ts
@@ -2,6 +2,14 @@ import {defineConfig} from 'vite'
 import copy from "rollup-plugin-copy";
 
 export default defineConfig({
+    build: {
+        commonjsOptions: {
+            include: []
+        }
+    },
+    optimizeDeps: {
+        disabled: false,
+    },
     plugins: [
         copy({
             targets: [{src: "openapi.json", dest: "dist/"}],
