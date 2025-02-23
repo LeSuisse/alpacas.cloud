c399095f4a6a7bb40991605599012bba513c3df2
Thomas Gerbet <thomas@gerbet.me>
2025-02-23 17:30:55 +0100

    chore: Bump JS deps

--- cmd/alpacascloud/web/vite.config.ts
+++ cmd/alpacascloud/web/vite.config.ts
@@ -1,19 +1,3 @@
 import {defineConfig} from 'vite'
-import copy from "rollup-plugin-copy";
 
-export default defineConfig({
-    build: {
-        commonjsOptions: {
-            include: []
-        }
-    },
-    optimizeDeps: {
-        disabled: false,
-    },
-    plugins: [
-        copy({
-            targets: [{src: "openapi.json", dest: "dist/"}],
-            hook: 'writeBundle'
-        })
-    ]
-});
+export default defineConfig({});
